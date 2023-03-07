b460225a863730945bfbd679232695ecd5281404
Subins11 <122017529+Subins11@users.noreply.github.com>
2023-03-07 12:04:16 +0530

    first commit

---
+++ Movieapp/src/App.js
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Search from "./components/Search";
+import AddMovie from "./components/AddMovie";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Edituser from "./components/Edituser";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Header" element={<Header />}></Route>
+        <Route path="/AddMovie" element={<AddMovie />}></Route>
+        <Route path="/Search" element={<Search />}></Route>
+        <Route path="/Edituser/:id" element={<Edituser />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
